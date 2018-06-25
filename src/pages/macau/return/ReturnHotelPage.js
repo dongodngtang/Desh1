3ef995c4cdeaf1e5295a3486fe48d9611236674f
lorne <2270333671@qq.com>
2018-06-25 10:53:53 +0800

    酒店退款页面

--- src/pages/macau/return/ReturnHotelPage.js
+++ src/pages/macau/return/ReturnHotelPage.js
@@ -3,33 +3,43 @@ import React, {Component} from 'react';
 import {View, StyleSheet, ScrollView, Text, Image, TouchableOpacity, Alert, ListView, TextInput} from 'react-native';
 import {Colors, Fonts, Images, ApplicationStyles, Metrics} from '../../../Themes';
 import {NavigationBar, BaseComponent} from '../../../components';
-import {postTempImg, postMallRefund} from '../../../services/MallDao';
-import {strNotNull, showToast, getFileName, util, alertOrder} from '../../../utils/ComonHelper';
 import ImageLoad from "../../../components/ImageLoad";
-import I18n from "react-native-i18n";
+import {showToast} from "../../../utils/ComonHelper";
+
 
 export default class ReturnHotelPage extends Component {
-
+    _notes = (item) => {
+        const {room_title, order} = item;
+        const {nights_num, room_num} = order;
+        return <View style={styles.notes}>
+            <Text style={{color: "#AAAAAA", fontSize: 12, marginRight: 12}}>{nights_num}晚</Text>
+            <Text style={{color: "#AAAAAA", fontSize: 12, marginRight: 12}}>{room_num}间</Text>
+            <Text style={{color: "#AAAAAA", fontSize: 12, marginRight: 12}}>{room_title}</Text>
+        </View>
+    };
 
     render(){
-        const {hotel_logo, hotel_title, order} = this.props.params.item;
+        const {item} = this.props.params;
+        const {hotel_logo, hotel_title, order} = item;
         const {checkin_date, checkout_date, status, total_price,order_number} = order;
+        let time = `${checkin_date}至${checkout_date}`;
 
         return(
             <View style={ApplicationStyles.bgContainer}>
                 <NavigationBar
-                    toolbarStyle={{
-                        position: 'absolute',
-                        top: 0,
-                        backgroundColor: 'rgba(229,74,46,' + this.state.opacity + ')'
-                    }}
+                    titleStyle={{fontSize: 17, color: Colors.white}}
+                    toolbarStyle={{backgroundColor: Colors._E54}}
                     title={'申请退换货'}
                     leftBtnIcon={Images.sign_return}
                     leftImageStyle={{height: 19, width: 11, marginLeft: 20, marginRight: 20}}
-                    leftBtnPress={() => router.pop()}/>
+                    leftBtnPress={() => {
+                        router.pop()
+                    }}
+                />
+
 
                 <View style={styles.itemView}>
-                    {this._line()}
+                    <View style={{height:1,backgroundColor:"#ECECEE",width:'100%'}}/>
                     <View style={styles.item}>
                         <ImageLoad
                             style={{width: 100, height: 96}}
@@ -37,35 +47,43 @@ export default class ReturnHotelPage extends Component {
                         <View style={styles.message}>
                             <Text style={{color: "#333333", fontSize: 16}}>{hotel_title}</Text>
                             <Text style={{color: "#666666", fontSize: 12, marginTop: 7}}>{time}</Text>
-                            {this._notes(this.props.item)}
+                            {this._notes(item)}
                         </View>
-                    </View>
-                    <View style={styles.returnPriceView}>
-                        <Text style={{color:'#333333',fontSize:14}}>退款金额：</Text>
-                        <Text style={{color:'#F24A4A',fontSize:18}}>¥1027823</Text>
-                    </View>
-                    <View style={styles.returnPriceView}>
-                        <Text style={{color:'#333333',fontSize:14}}>退款说明：</Text>
-                        <TextInput
-                            style={styles.inputText}
-                            onChangeText={text => {
-                                this.setState({
-                                    memo: text
-                                })
-                            }}
-                            underlineColorAndroid='transparent'
-                            numberOfLines={1}
-                        />
                     </View>
 
                 </View>
+
+                <View style={styles.returnPriceView}>
+                    <Text style={{color:'#333333',fontSize:14,marginLeft:17}}>退款金额：</Text>
+                    <Text style={{color:'#F24A4A',fontSize:18}}>¥1027823</Text>
+                </View>
+                <View style={styles.returnPriceView}>
+                    <Text style={{color:'#333333',fontSize:14,marginLeft:17}}>退款说明：</Text>
+                    <TextInput
+                        style={styles.inputText}
+                        onChangeText={text => {
+                            // this.setState({
+                            //     memo: text
+                            // })
+                        }}
+                        underlineColorAndroid='transparent'
+                        numberOfLines={1}
+                    />
+                </View>
+
+                <TouchableOpacity style={{position: 'absolute',width:'100%',paddingTop:13,paddingBottom:13,alignItems:'center',justifyContent:'center',backgroundColor:"#E54A2E",bottom:0}}
+                onPress={()=>{
+                    showToast('申请提交成功');
+                    global.router.toMallOrderPage();
+                }}>
+                    <Text style={{color:'#FFFFFF',fontSize:18}}>提交</Text>
+                </TouchableOpacity>
             </View>
         )
     }
 }
 const styles = StyleSheet.create({
     inputText: {
-        height: 40,
         fontSize: 14,
         flex: 1,
         marginTop: 5
