98c9f2408fa1439ff16b33a010f1282fa9862b70
lorne <2270333671@qq.com>
2018-07-04 17:34:45 +0800

    汇率

--- src/pages/macau/RatePage.js
+++ src/pages/macau/RatePage.js
@@ -6,30 +6,73 @@ import {Colors, Fonts, Images, ApplicationStyles, Metrics} from '../../Themes';
 import {NavigationBar} from '../../components';
 import {LoadErrorView, NoDataView} from '../../components/load';
 import {getExchange_rates} from '../../services/MacauDao';
-import {isEmptyObject} from "../../utils/ComonHelper";
+import {isEmptyObject, mul,div} from "../../utils/ComonHelper";
 
-const groups = [{id: 1, img: Images.cny, abb: 'CNY', name: '人民币¥'},
-    {id: 2, img: Images.hkd, abb: 'HKD', name: '港币$'},
-    {id: 3, img: Images.mop, abb: 'MOP', name: '澳门币$'}];
+const groups = [{id: 0, img: Images.cny, abb: 'CNY', name: '人民币¥'},
+    {id: 1, img: Images.hkd, abb: 'HKD', name: '港币$'},
+    {id: 2, img: Images.mop, abb: 'MOP', name: '澳门币$'}];
 
 export default class RatePage extends Component {
 
     state = {
-        ratesItem: {}
+        ratesItem: {},
+        price_changed: groups,
+        show: false
     }
 
     componentDidMount() {
+        let rate = [100, 0, 0];
+        const {price_changed} = this.state;
         getExchange_rates(data => {
             console.log("ratesItem:", data)
+            rate[1] = mul(data.cny_to_hkd_rate.rate, rate[0]);
+            rate[2] = mul(data.cny_to_mop_rate.rate, rate[0]);
+            let group2 = price_changed;
+            group2.map((item, index) => {
+                item.price = rate[index]
+            });
+
             this.setState({
-                ratesItem: data
+                ratesItem: data,
+                price_changed: group2
             })
         }, err => {
 
         })
     }
 
+    changing_price = (item, index, txt) => {
+        console.log("shjdjfs",index)
+        console.log("shjdjfs",item)
+        const {price_changed} = this.state;
+        const {cny_to_hkd_rate, cny_to_mop_rate} = this.state.ratesItem;
+        let group2 = price_changed;
+        let rate = [0, 0, 0];
+        if (index === 0) {
+            rate[0] = txt;
+            rate[1] = mul(rate[0], cny_to_hkd_rate.rate);
+            rate[2] = mul(rate[0], cny_to_mop_rate.rate);
+        } else if (index === 1) {
+            rate[1] = txt;
+            rate[0] = div(rate[1],cny_to_hkd_rate.rate);
+            rate[2] = mul(rate[0], cny_to_mop_rate.rate);
+        } else if (index === 2) {
+            rate[2] = txt;
+            rate[0] = div(rate[2],cny_to_mop_rate.rate);
+            rate[1] = mul(rate[0], cny_to_hkd_rate.rate);
+        }
+        group2.map((x, index) => {
+            x.price = rate[index]
+        });
+        this.setState({
+            price_changed: group2
+        })
+    }
+    ;
+
+
     render() {
+        const {show} = this.state;
         const {cny_to_hkd_rate, cny_to_mop_rate} = this.state.ratesItem;
 
         if (isEmptyObject(cny_to_hkd_rate) && isEmptyObject(cny_to_mop_rate)) {
@@ -56,37 +99,50 @@ export default class RatePage extends Component {
 
                 <View style={styles.page}>
                     <Text style={styles.txt}>今日汇率：</Text>
-                    <View style={{flexDirection: 'row'}}>
-                        <Text>{`1人名币=1.2101港币，1港币=${cny_to_hkd_rate.rate}人名币`}</Text>
-                        <Text>{`1人名币=1.2101澳门币，1澳门币=${cny_to_mop_rate.rate}人名币`}</Text>
+                    <View style={{flexDirection: 'column', alignSelf: 'center'}}>
+                        <Text style={styles.txt}>{`1人名币=1.2101港币，1港币=${cny_to_hkd_rate.rate}人名币`}</Text>
+                        <Text
+                            style={[styles.txt, {marginTop: 5}]}>{`1人名币=1.2101澳门币，1澳门币=${cny_to_mop_rate.rate}人名币`}</Text>
                     </View>
                 </View>
 
-                <View style={{flexDirection: 'row'}}>
-                    {groups.map((item) => {
-                        return (
-                            <View style={styles.itemPage}>
+                {groups.map((item, index) => {
+                    return (
+                        <View style={{flexDirection: 'column', marginLeft: 17, marginRight: 17}} key={index}>
+                            <View style={styles.itemPage} key={index}>
                                 <Image style={{width: 44, height: 44}} source={item.img}/>
                                 <Text style={{color: "#444444", fontSize: 18, marginLeft: 18}}>{item.abb}</Text>
                                 <View style={{flex: 1}}/>
                                 <View style={{flexDirection: 'column', alignItems: 'flex-end'}}>
                                     <TextInput
                                         keyboardType={'numeric'}
-                                        style={{paddingTop: 0, paddingBottom: 0}}
-                                        maxLength={12}
+                                        style={{
+                                            paddingTop: 0,
+                                            paddingBottom: 0,
+                                            width: 100,
+                                            fontSize: 24,
+                                            fontWeight: 'bold',
+                                            alignItems:'flex-end'
+                                        }}
+                                        maxLength={20}
                                         numberOfLines={1}
+                                        placeholderTextColor={show ? '#444444' : '#F3F3F3'}
+                                        placeholder={item.price + ''}
                                         underlineColorAndroid={'transparent'}
                                         onChangeText={txt => {
-
+                                            this.setState({
+                                                show: true
+                                            })
+                                            this.changing_price(item, index, txt)
                                         }}
-                                        value={phone}/>
+                                    />
                                     <Text style={{color: "#8C8C8C", fontSize: 14, marginTop: 6}}>{item.name}</Text>
                                 </View>
                             </View>
-                        )
-                    })}
-
-                </View>
+                            <View style={{height: 1.5, width: '100%', backgroundColor: "#F3F3F3"}}/>
+                        </View>
+                    )
+                })}
             </View>
         )
     }
@@ -97,15 +153,14 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         alignItems: 'center',
         paddingTop: 23,
-        paddingBottom: 21,
-        marginLeft: 17,
-        marginRight: 17
+        paddingBottom: 21
     },
     page: {
+        width: '100%',
         paddingTop: 12,
         paddingBottom: 9,
-        marginLeft: 17,
-        marginRight: 17,
+        paddingLeft: 17,
+        paddingRight: 17,
         flexDirection: 'row',
         backgroundColor: "#F3F3F3"
     },
--- src/utils/ComonHelper.js
+++ src/utils/ComonHelper.js
@@ -116,6 +116,18 @@ export function mul(num1, num2) {
     return Number(s1.replace(".", "")) * Number(s2.replace(".", "")) / Math.pow(10, m)
 }
 
+export function div(a, b) {
+    var c, d, e = 0,
+        f = 0;
+    try {
+        e = a.toString().split(".")[1].length;
+    } catch (g) {}
+    try {
+        f = b.toString().split(".")[1].length;
+    } catch (g) {}
+    return c = Number(a.toString().replace(".", "")), d = Number(b.toString().replace(".", "")), mul(c / d, Math.pow(10, f - e));
+}
+
 
 export function localFilePath(path) {
     if (Platform.OS === 'android')
