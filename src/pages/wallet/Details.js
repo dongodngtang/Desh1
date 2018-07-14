c1bbdc168245e5d7b9497e89a93fa243c4f27830
lorne <2270333671@qq.com>
2018-07-14 14:58:11 +0800

    详情宽度

--- src/pages/wallet/Details.js
+++ src/pages/wallet/Details.js
@@ -95,7 +95,7 @@ const styles = StyleSheet.create({
         alignItems: 'center'
     },
     itemLeft: {
-        width:'75%',
+        width:'70%',
         flexDirection: 'column',
         marginLeft: 17
     }
