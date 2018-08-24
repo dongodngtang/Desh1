3a013a899dbc66d808460e9b56c00e7cb1a6d8df
lorne <2270333671@qq.com>
2018-08-24 16:05:34 +0800

    图片保存 未完成

--- src/components/ImageGallery.js
+++ src/components/ImageGallery.js
@@ -3,23 +3,60 @@
  */
 import React, {PropTypes} from 'react';
 import {
-    Modal, ActivityIndicator,View
+    Modal, ActivityIndicator, View, CameraRoll, Platform
 } from 'react-native';
 import ImageViewer from 'react-native-image-zoom-viewer';
 import {Colors, Fonts, Images, ApplicationStyles, Metrics} from '../Themes';
-import {showToast} from "../utils/ComonHelper";
+import {logMsg, showToast} from "../utils/ComonHelper";
+import RNFS from 'react-native-fs';
+
+const storeLocation = `${RNFS.DocumentDirectoryPath}`;
 
 export default class ImageGallery extends React.Component {
 
 //保存图片
-//     saveImg=(img)=> {
-//         var promise = CameraRoll.saveToCameraRoll(img);
-//         promise.then(function(result) {
-//             showToast('保存成功！');
-//         }).catch(function(error) {
-//             showToast('保存失败！');
-//         });
-//     };
+    saveImg = (img) => {
+        let path = img.url;
+        if (Platform.OS === 'ios') {
+            var promise = CameraRoll.saveToCameraRoll(path);
+            promise.then(function (result) {
+                showToast('保存成功！');
+            }).catch(function (error) {
+                showToast('保存失败！');
+            });
+        } else {
+            let filename;
+            if (path.indexOf("/") > 0)//如果包含有"/"号 从最后一个"/"号+1的位置开始截取字符串
+            {
+                filename = path.substring(img.url.lastIndexOf("."), path.length);
+            }
+            else {
+                filename = path;
+            }
+            logMsg('filename',filename)
+            let pathName = new Date().getTime() + filename;
+            let downloadDest = `${storeLocation}/${pathName}`;
+            logMsg('downloadDest',downloadDest)
+            try {
+                const ret = RNFS.downloadFile({fromUrl: path, toFile: downloadDest,background: true});
+                ret.promise.then(res => {
+                    CameraRoll.saveToCameraRoll(downloadDest)
+                        .then(()=>{
+                            logMsg('图片已保存到相册')
+                        }).catch(()=>{
+                        logMsg('图片保存失败')
+                    })
+
+                }).catch(err => {
+                    console.log('err', err);
+                });
+            }
+            catch (e) {
+                console.log(error);
+            }
+        }
+
+    };
 
     render() {
 
@@ -31,10 +68,9 @@ export default class ImageGallery extends React.Component {
                         color='white'/>
                 }}
                 // saveToLocalByLongPress={true}
-                // onLongPress={(image) => {
-                //     console.log('image', image);
-                //     // this.saveImg(image.url)
-                // }}
+                onLongPress={(image) => {
+                    this.saveImg(image);
+                }}
                 imageUrls={images}
                 index={index}
                 onClick={() => router.pop()}/>
