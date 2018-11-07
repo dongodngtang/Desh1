c024eb9623fdb5776864f75cdd78c13035867ff1
lorne <2270333671@qq.com>
2018-11-07 14:42:19 +0800

    签证页面修改

--- src/pages/fastBtn/VisaInfoPage.js
+++ src/pages/fastBtn/VisaInfoPage.js
@@ -23,18 +23,18 @@ export default class VisaInfoPage extends Component {
                         router.pop();
                     }}
                 />
-                <View style={{flex:1,marginTop:1,backgroundColor:'white',paddingLeft:17,paddingRight:17}}>
-                    <Text style={[styles.txt1,{marginTop:20}]}>港澳通行证团队旅游L签续签办理</Text>
-                    <Text style={[styles.txt2,{marginTop:20}]}>合作商铺地址：珠海市香洲区拱北地下口岸广场t字通道
+                <View style={{flex: 1, marginTop: 1, backgroundColor: 'white', paddingLeft: 17, paddingRight: 17}}>
+                    <Text style={[styles.txt1, {marginTop: 20}]}>港澳通行证团队旅游L签续签办理</Text>
+                    <Text style={[styles.txt2, {marginTop: 20}]}>合作商铺地址：珠海市香洲区拱北地下口岸广场t字通道
                     </Text>
-                    <Text style={[styles.txt2,{marginTop:5}]}>营业时间：11:30 至20:30
+                    <Text style={[styles.txt2, {marginTop: 5}]}>营业时间：11:30 至20:30
                     </Text>
 
                     <TouchableOpacity onPress={() => {
                         let phone = "13169674979"
                         call(phone);
                     }}>
-                        <Text style={[styles.txt2,{marginTop:5}]}>请联系电话：+86-13169674979</Text>
+                        <Text style={[styles.txt2, {marginTop: 5}]}>请联系电话：+86-13169674979</Text>
                     </TouchableOpacity>
 
                     <TouchableOpacity onPress={() => {
@@ -44,8 +44,11 @@ export default class VisaInfoPage extends Component {
                             global.router.toMallInfoPage({id: 67})
                         }
 
-                    }}>
-                        <Text style={[styles.txt3,{marginTop:36,textDecorationLine:'underline',alignSelf:'center'}]}>快签办理点击前往>></Text>
+                    }}
+                                      style={[styles.btn,{backgroundColor:"#E54A2E",marginTop: 36}]}>
+                        <Text style={[styles.txt3, {
+                            alignSelf: 'center'
+                        }]}>快签办理点击前往>></Text>
                     </TouchableOpacity>
                     <TouchableOpacity onPress={() => {
                         if (getApiType() === 'test') {
@@ -53,8 +56,11 @@ export default class VisaInfoPage extends Component {
                         } else {
                             global.router.toMallInfoPage({id: 35})
                         }
-                    }}>
-                        <Text style={[styles.txt3,{marginTop:20,textDecorationLine:'underline',alignSelf:'center'}]}>慢签办理点击前往>></Text>
+                    }}
+                                      style={[styles.btn,{backgroundColor:"#4A90E2",marginTop: 20}]}>
+                        <Text style={[styles.txt3, {
+                            alignSelf: 'center'
+                        }]}>慢签办理点击前往>></Text>
                     </TouchableOpacity>
                 </View>
             </View>
@@ -69,12 +75,20 @@ const styles = StyleSheet.create({
         fontSize: 18,
         fontWeight: 'bold'
     },
-    txt2:{
+    txt2: {
         color: '#444444',
         fontSize: 14
     },
-    txt3:{
-        color: '#4A90E2',
+    txt3: {
+        color: '#FFFFFF',
         fontSize: 18
+    },
+    btn: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginLeft: 17,
+        marginRight: 17,
+        borderRadius:2,
+        height:48
     }
 })
